d73a97bf6b1eb6e7510eb9836b8eb3e28191dc46
sandipsky <sandipshakya75@gmail.com>
2026-03-06 17:53:36 +0545

    header

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { BreadcrumbService } from '../../shared/services/breadcrumb.service';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { MenuComponent } from '../../shared/components/menu/menu';
+import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-header',
@@ -13,10 +14,26 @@ import { MenuComponent } from '../../shared/components/menu/menu';
 })
 export class Header {
   private _breadcrumbService = inject(BreadcrumbService);
+  private _router = inject(Router);
+  // private _sidebar = inject(Sidebar);
 
   breadcrumbs = toSignal(this._breadcrumbService.breadcrumbs$);
 
   userName = signal<string>('Sandip Shakya');
   role = signal<string>('Admin');
   fiscalYear = signal<string>('2082-83');
+  notifications = signal<any[]>([]);
+
+  returnHome() {
+    this._router.navigate(['dashboard']);
+    // this._sidebar.setActiveMenuFromUrl();
+  }
+
+  clearAllNotifications() {
+
+  }
+
+  clearNotification(id: number) {
+
+  }
 }
