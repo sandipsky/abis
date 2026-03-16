c61961638d7265f79523ae19a3f3aee209c3e3f1
sandipsky <sandipshakya75@gmail.com>
2026-03-16 13:13:33 +0545

    changes

--- src/app/modules/dashboard/dashboard.ts
+++ src/app/modules/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FilterSectionComponent } from '../../shared/components/filter/filter';
+import { PaginatorComponent } from '../../shared/components/pagination/pagination';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [FilterSectionComponent],
+  imports: [FilterSectionComponent, PaginatorComponent],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
--- src/app/shared/components/pagination/pagination.ts
+++ src/app/shared/components/pagination/pagination.ts
@@ -1,11 +1,87 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MenuComponent } from '../menu/menu';
 
 @Component({
-  selector: 'app-pagination',
-  imports: [],
+  selector: 'app-paginator',
   templateUrl: './pagination.html',
-  styleUrl: './pagination.scss',
+  styleUrls: ['./pagination.scss'],
+  standalone: true,
+  imports: [CommonModule, MenuComponent]
 })
-export class Pagination {
+export class PaginatorComponent {
+  @Input() pageSizeOptions: number[] = [10, 25, 50, 100];
+  @Input() pageSize: number = 10;
+  @Input() pageIndex: number = 0;
+  @Input() length: number = 0;
 
+  @Output() pageChange: EventEmitter<{ pageIndex: number, pageSize: number, length: number }> = new EventEmitter();
+
+  visiblePages: number[] = [];
+
+  isOpen: boolean = false
+  openTop = false;
+
+  ngOnChanges() {
+    this.updateVisiblePages();
+  }
+
+  onPageChange(): void {
+    this.pageChange.emit({ pageIndex: this.pageIndex, pageSize: this.pageSize, length: this.length });
+  }
+
+  onchangePageOption(page: number) {
+    this.pageSize = page;
+    this.updateVisiblePages();
+    this.onPageChange();
+  }
+
+  onPrevAndNext(direction: string) {
+    direction == 'prev' ? this.pageIndex -= 1 : this.pageIndex += 1;
+    this.updateVisiblePages();
+    this.onPageChange();
+  }
+
+  goFirstPage() {
+    this.pageIndex = 0;
+    this.updateVisiblePages();
+    this.onPageChange();
+  }
+
+  goLastpage() {
+    this.pageIndex = this.calculateTotalPages() - 1;
+    this.updateVisiblePages();
+    this.onPageChange();
+  }
+
+  updateVisiblePages() {
+    const totalPages = this.calculateTotalPages();
+    const current = this.pageIndex + 1;
+
+    let start = Math.max(current - 2, 1);
+    let end = start + 4;
+
+    if (end > totalPages) {
+      end = totalPages;
+      start = Math.max(end - 4, 1);
+    }
+
+    this.visiblePages = Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  }
+
+  goToPage(index: number) {
+    this.pageIndex = index;
+    this.updateVisiblePages();
+    this.onPageChange();
+  }
+
+  calculateTotalPages(): number {
+    return this.length > 0 ? Math.ceil(this.length / this.pageSize) : 1;
+  }
+
+  getRangeLabel(): any {
+    if (this.length === 0) return 0;
+    const remainingItems = this.length - (this.pageIndex * this.pageSize);
+    return Math.min(this.pageSize, remainingItems);
+  }
 }
