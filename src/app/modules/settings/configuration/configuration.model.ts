71a6c79904651350832ee726ffffb9ed9913bfa6
sandipsky <sandipshakya75@gmail.com>
2026-05-04 19:18:53 +0545

    config temp

---
+++ src/app/modules/settings/configuration/configuration.model.ts
@@ -0,0 +1,15 @@
+export interface configurationModel {
+  id: number,
+  config_name: string,
+  config_value: string,
+  config_description: string,
+}
+
+
+export interface configurationData {
+  id: number,
+  name: string,
+  value: string,
+  description: string,
+}
+
